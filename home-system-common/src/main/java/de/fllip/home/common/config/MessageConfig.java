ecbacdb60989419ea92dcda3277e238243cf1318
Philipp <p.eistrach@gmail.com>
2022-10-27 19:36:40 +0200

    refactor: add messages

--- home-system-common/src/main/java/de/fllip/home/common/config/MessageConfig.java
+++ home-system-common/src/main/java/de/fllip/home/common/config/MessageConfig.java
@@ -9,14 +9,18 @@ package de.fllip.home.common.config;
 public record MessageConfig(
         String homeInventoryTitle,
         String homeItemDescription,
-        String deleteHomeItemName
+        String deleteHomeItemName,
+        String noPlayerWarningMessage,
+        String noNameProvidedMessage
 ) {
 
     public MessageConfig() {
         this(
                 "<green>Deine Homes</green>",
                 "<gray>Klicke, um dich zum Home zu teleportieren</gray> <newline> <newline> <gray>Server: <yellow><server></yellow>",
-                "<red>Alle Homes löschen</red>"
+                "<red>Alle Homes löschen</red>",
+                "<red>Du musst ein Spieler sein!</red>",
+                "<red>Bitte gib einen Namen an!</red>"
         );
     }
 
