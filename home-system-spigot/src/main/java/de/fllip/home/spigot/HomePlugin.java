6c5862969ebec047e6da9afa11332484138e7e42
Philipp <p.eistrach@gmail.com>
2022-10-27 19:39:33 +0200

    implement: commands are now initialized

--- home-system-spigot/src/main/java/de/fllip/home/spigot/HomePlugin.java
+++ home-system-spigot/src/main/java/de/fllip/home/spigot/HomePlugin.java
@@ -5,8 +5,10 @@ import de.fllip.home.api.repository.HomeRepository;
 import de.fllip.home.common.DefaultHomeAPI;
 import de.fllip.home.common.config.Config;
 import de.fllip.home.common.config.ConfigLoader;
+import de.fllip.home.common.config.MessageConfig;
 import de.fllip.home.common.database.DataSourceFactory;
 import de.fllip.home.common.repository.DefaultHomeRepository;
+import de.fllip.home.spigot.commands.*;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -39,6 +41,18 @@ public class HomePlugin extends JavaPlugin {
                     this.getLogger().log(Level.WARNING, "Error while creating 'homes' table");
                     return null;
                 });
+
+        this.initializeCommands();
+    }
+
+    private void initializeCommands() {
+        CommandInitializer commandInitializer = new CommandInitializer(this);
+        MessageConfig messageConfig = this.config.messageConfig();
+
+        commandInitializer.initializeCommand("homes", new HomesCommand(this.homeAPI, messageConfig));
+        commandInitializer.initializeCommand("home", new HomeCommand(this.homeAPI, messageConfig));
+        commandInitializer.initializeCommand("sethome", new SetHomeCommand(this.homeAPI, messageConfig));
+        commandInitializer.initializeCommand("delhome", new DeleteHomeCommand(this.homeAPI, messageConfig));
     }
 
 }
